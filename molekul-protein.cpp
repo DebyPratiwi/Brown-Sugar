afa67c06244d96556b6b035228b8351678ae2114
DebyPratiwi <106718730+DebyPratiwi@users.noreply.github.com>
2022-06-03 23:27:14 +0700

    Add files via upload
    progress pertama

---
+++ molekul-protein.cpp
@@ -0,0 +1,35 @@
+
+#iclude "iostream"
+#include "cmath"
+#include "iomanip"
+#include "fstream"
+
+using namespace std;
+
+int main (){
+    // deklarasi variabel 
+    float pi = 3.14;
+
+    //deklarasi molekul protein 
+    float Rx, Ry, Rz; 
+    float C1x, C1y, C1z;
+    float C2x, C2y, C2z;
+    float H1x, H1y, H1z;
+    float H2x, H2y, H2z;
+    float H3x, H3y, H3z;
+    float H4x, H4y, H4z;
+    float Nx, Ny, Nz;
+    float O1x, O1y, O1z;
+    float O2x, O2y, O2z;
+
+    /*          Coo-
+                 |
+          H3N+ - C - H 
+                 |
+                 R 
+          molekul protein itu bentuk rigid 
+        */
+        
+
+
+}
