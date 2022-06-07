0e1b21ff4e6fcd3213e9a1634a7129c53d1ac19c
MzDediWahyu <106825593+MzDediWahyu@users.noreply.github.com>
2022-06-07 17:43:18 +0700

    Update molekul-protein.cpp

--- molekul-protein.cpp
+++ molekul-protein.cpp
@@ -65,6 +65,45 @@ int main (){
         float rB_O2y = 0.0;
         float rB_O3z = cos((masukan nilai /2.0)*pi/180.0) * 1.090;
     
-    
+    // iterasi untuk setiap molekul protein
+    for(int a = 1; a = < 10; a++){
+        for(int b = 1; b = < 10; b++){
+            for(int c = 1; c = < 10; c++){
+                
+                C1x[counter] = rB_C1x + (a*3.01);
+                C1y[counter] = rB_C1y + (b*3.01);
+                C1z[counter] = rB_C1z + (c*3.01);
+                
+                C2x[counter] = C1x[counter] + rB_C2x;
+                C2y[counter] = C1y[counter] + rB_C2y;
+                C2z[counter] = C1z[counter] + rB_C2z;
+                
+                H1x[counter] = C1x[counter] + rB_H1x;
+                H1y[counter] = C1y[counter] + rB_H1y;
+                H1z[counter] = C1z[counter] + rB_H1z;
+                
+                H2x[counter] = C1x[counter] + rB_H2x;
+                H2y[counter] = C1y[counter] + rB_H2y;
+                H2z[counter] = C1z[counter] + rB_H2z;
+                
+                H3x[counter] = C1x[counter] + rB_H3x;
+                H3y[counter] = C1y[counter] + rB_H3y;
+                H3z[counter] = C1z[counter] + rB_H3z;
+                
+                H4x[counter] = C1x[counter] + rB_H4x;
+                H4y[counter] = C1y[counter] + rB_H4y;
+                H4z[counter] = C1z[counter] + rB_H4z;
+                
+                Nx[counter] = C1x[counter] + rB_Nx;
+                Ny[counter] = C1y[counter] + rB_Ny;
+                Nz[counter] = C1z[counter] + rB_Nz;
+                
+                O1x[counter] = C1x[counter] + rB_O1x;
+                O1y[counter] = C1y[counter] + rB_O1y;
+                O1z[counter] = C1z[counter] + rB_O1z;
+                
+                O2x[counter] = C1x[counter] + rB_O2x;
+                O2y[counter] = C1y[counter] + rB_O2y;
+                O2z[counter] = C1z[counter] + rB_O2z;
 
 }
